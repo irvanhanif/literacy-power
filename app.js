24ee50b28fd71bc766f6d7c252514fd85d9b27a6
irvanhanif24 <irhan8803@gmail.com>
2022-08-28 19:41:00 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+
+const port = process.env.PORT || 4500;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('', (req, res) => {
+    res.json("aman");
+});
+
+app.listen(port, () => {
+    console.log(`app run at port ${port}`);
+});

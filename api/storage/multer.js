df6e6fc6711e21c5c556c3033bcabcbe49504446
irvanhanif24 <irhan8803@gmail.com>
2022-10-01 15:00:52 +0700

    update controller

--- api/storage/multer.js
+++ api/storage/multer.js
@@ -2,15 +2,15 @@ const multer = require("multer");
 const path = require('path');
 
 const upload = multer({
-    storage: multer.diskStorage({}),
-    fileFilter: (req, file, cb) => {
-        let ext = path.extname(file.originalname);
-        if(ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
-            cb(new Error("File type is not supported"), false);
-            return;
-        }
-        cb(null, true);
-    }
+    storage: multer.diskStorage({})
+    // fileFilter: (req, file, cb) => {
+    //     let ext = path.extname(file.originalname);
+    //     if(ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
+    //         cb(new Error("File type is not supported"), false);
+    //         return;
+    //     }
+    //     cb(null, true);
+    // }
 }).single('foto');
 
 module.exports = upload

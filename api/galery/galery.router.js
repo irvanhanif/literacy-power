62865b2226c617af2bf5848eaaf0f6b0ec674c53
irvanhanif24 <irhan8803@gmail.com>
2022-10-01 15:35:12 +0700

    update controller

--- api/galery/galery.router.js
+++ api/galery/galery.router.js
@@ -4,12 +4,13 @@ const { tokenValidation } = require('../middleware');
 const {
     postPhoto,
     allPhoto,
-    deletePhoto
+    deletePhoto,
+    getPhoto
 } = require('./galery.controller');
 const upload = require('../storage/multer');
 
 router.get('/', allPhoto);
-// router.get('/:id', );
+router.get('/:id', tokenValidation, getPhoto);
 router.post('/', tokenValidation,upload, postPhoto);
 router.delete('/:id', tokenValidation, deletePhoto);
 
